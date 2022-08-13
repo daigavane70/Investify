b5e81fe29b57af6536404332760cc5f61d3bf60b
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-08-13 15:05:35 +0530

    router configured

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import AllStartups from "./pages/AllStartups";
+import Bids from "./pages/Bids";
+import Home from "./pages/Home";
+import Messaging from "./pages/Messaging";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav></nav>
+      <>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </>
+      <Routes>
+        <Route path="/all" element={<AllStartups></AllStartups>}></Route>
+        <Route path="/bids" element={<Bids></Bids>}></Route>
+        <Route path="/messaging" element={<Messaging></Messaging>}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ client/src/pages/AllStartups.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AllStartups() {
+  return <div>AllStartups</div>;
+}
